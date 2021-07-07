47eaf1434f9daa131544442226b7854f95ee87dd
ROman Rudyk <r.a.rudyk@gmail.com>
2021-07-08 00:10:41 +0300

    creating solution for task fron four to seven

--- HW5_Classes_lecture_2.py
+++ HW5_Classes_lecture_2.py
@@ -59,3 +59,100 @@ print(calc)
     Make class with one method "add_nums" with 3 parameters, which returns sum of these parameters.
     Note: this method should be static
 """
+
+
+#4*.
+print('\nTask 4')
+class Pasta:
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+#    def __str__(self):
+#        return f"Pasta includes ingredients: {self.ingredients}"
+    def __repr__(self):
+        return f"Pasta ({self.ingredients})"
+    @classmethod
+    def carbonara(cls, ingredient):
+        i = ['forcemeat', 'tomatoes'].append(ingredient)
+        return cls(i)
+    @classmethod
+    def bolognaise(cls):
+        d = ['bacon', 'parmesan', 'eggs']
+        return cls(d)
+
+pasta_1 = Pasta(["tomato", "cucumber"])
+print(pasta_1)
+pasta_2 = Pasta.bolognaise()
+print(pasta_2)
+"""
+    Make class which takes 1 parameter on init - list of ingredients and defines instance attribute ingredients.
+    It should have 2 methods:
+    carbonara (['forcemeat', 'tomatoes']) and bolognaise (['bacon', 'parmesan', 'eggs'])
+    which should create Pasta instances with predefined list of ingredients.
+    Example:
+        pasta_1 = Pasta(["tomato", "cucumber"])
+        pasta_1.ingredients will equal to ["tomato", "cucumber"]
+        pasta_2 = Pasta.bolognaise()
+        pasta_2.ingredients will equal to ['bacon', 'parmesan', 'eggs']
+"""
+
+#5*.
+print('\nTask 5')
+class Concert:
+    max_visitor_num = 0
+    def __init__(self, visitors_count = 0):
+        self.visitors_count = visitors_count
+    @property
+    def visitors_count(self):
+        return f"Max visitors number: {self._visitors_count}"
+
+    @visitors_count.setter
+    def visitors_count(self, v_count):
+        if v_count > self.max_visitor_num:
+            self._visitors_count = self.max_visitor_num
+        else:
+            self._visitors_count = v_count
+
+
+Concert.max_visitor_num = 50
+concert = Concert()
+concert.visitors_count = 1000
+print(concert.visitors_count)
+
+"""
+    Make class, which has max_visitors_num attribute and its instances will have visitors_count attribute.
+    In case of setting visitors_count - max_visitors_num should be checked,
+    if visitors_count value is bigger than max_visitors_num - visitors_count should be assigned with max_visitors_num.
+    Example:
+        Concert.max_visitor_num = 50
+        concert = Concert()
+        concert.visitors_count = 1000
+        print(concert.visitors_count)  # 50
+"""
+
+#6.
+print('\nTask 6')
+import dataclasses
+
+@dataclasses.dataclass
+class AddressBookDataClass:
+    key: int
+    name: str
+    phone_number :str
+    address: str
+    email: str
+    birthday :str
+    age: int
+
+"""
+    Create dataclass with 7 fields - key (int), name (str), phone_number (str), address (str), email (str), birthday (str), age (int)
+"""
+
+#7. Create the same class (6) but using NamedTuple
+print('\nTask 7')
+import collections
+
+AddressBookDataClass = collections.namedtuple('AddressBookDataClass',
+                                             ['key', 'name', 'phone_number', 'address',
+                                              'email', 'birthday', 'age'])
+
+
