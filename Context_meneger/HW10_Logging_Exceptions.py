5c28d5c08948831d238b1cef032d49b5ea8c0bb7
ROman Rudyk <r.a.rudyk@gmail.com>
2021-07-24 20:19:57 +0300

    create class

--- Context_meneger/HW10_Logging_Exceptions.py
+++ Context_meneger/HW10_Logging_Exceptions.py
@@ -268,10 +268,75 @@ class AlmostFilledGarbage(Exception):
     pass
 
 class VacuumRob:
-    energy_loss = 1
-    water_loss = 1
+    energy_loss = 10
+    water_loss = 10
 
     def __init__(self, battery_charge_level, garbage_fullness, water_level):
         self.battery_charge_level = battery_charge_level
         self.garbage_fullness = garbage_fullness
         self.water_level = water_level
+
+    def wash(self):
+        if self.water_level == 0:
+            raise NoWater
+        self.water_left = max(self.water_level - self.water_loss, 0)
+
+    def vacuum_cleaner(self):
+        self.garbage_fullness = min(self.garbage_fullness + random.randint(1, 3), 100)
+        if self.garbage_fullness > 90:
+            raise AlmostFilledGarbage
+        if self.garbage_fullness == 100:
+            print("Container is full")
+            input('Press "Enter" to clean container:')
+            self.garbage_fullness = 0
+
+    def step_end(self):
+        self.battery_charge_level = max(self.battery_charge_level - self.energy_loss, 0)
+        if self.battery_charge_level == 0:
+            raise NoPower
+        elif self.battery_charge_level < 20:
+            raise LowPower
+
+
+def wash(robot):
+    print('wash')
+    robot.wash()
+
+
+def vacuum_cleaner(robot):
+    print('vacuum_cleaner')
+    robot.vacuum_cleaner()
+
+
+work_robot = VacuumRob(100, 30, 80)
+
+def move(robot):
+    i = 10
+    k = 0
+    while True:
+        print("move")
+        try:
+            wash(robot)
+        except NoWater:
+            if k == 0:
+                print("There is no water, no more washing...")
+                k += 1
+        try:
+            vacuum_cleaner(robot)
+        except AlmostFilledGarbage:
+            print('Container is almost full, please clear!')
+        try:
+            robot.step_end()
+        except NoPower:
+            print("Battery empty, power off")
+            break
+        except LowPower:
+            if i == 0:
+                print("The work is stopped! Charge me!")
+                break
+            else:
+                print(f"Battery low, {i} steps remaining")
+                i = i - 1
+        time.sleep(1)
+
+move(work_robot)
