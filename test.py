69aa3a60967bd3a4af90c46cb37649c8336e52aa
ROman Rudyk <r.a.rudyk@gmail.com>
2021-08-02 13:07:41 +0300

    start work with homework "Decorators"

--- test.py
+++ test.py
@@ -1,6 +1,37 @@
+import requests
 import pytest
-from part_1_doctest import Calc
+
+URL = "http://api.zippopotam.us/us/90210"
+FAIL_URL = "http://api.zippopotam.us/us/90211"
+
 
 @pytest.fixture()
-def test_sum():
-    assert Calc.sum(1, 1) == 2
+def get_res():
+    response = requests.get(URL)
+    yield response
+
+
+@pytest.fixture()
+def get_fail_res():
+    response = requests.get(FAIL_URL)
+    yield response
+
+
+def test_oo1(get_res):
+    response = get_res
+
+    assert response.status_code == 200
+
+    body = response.json()
+    assert body["post code"] == '90210'
+    assert body["country"] == 'United States'
+
+
+def test_oo2(get_fail_res):
+    response = get_fail_res
+    assert response.status_code == 404
+    body = response.json()
+
+
+# if __name__ == '__main__':
+#     test_oo1()
