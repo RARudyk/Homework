671ff81f94a20db0074ce52c63d0cafb5bff689b
RARudyk <r.a.rudyk@gmail.com>
2022-02-10 18:18:32 +0200

    learning python restart day 3 for - loop

---
+++ test_start/day_3.py
@@ -0,0 +1,40 @@
+# for - loop (відрізняється від while тим,
+# що використовується коли відоме МАКС значення )
+
+for x in range(0, 5):
+    print('sweet')
+
+for z in range(3, 7):
+    print('sweet %s' % z)
+
+print(list(range(0, 5)))
+
+for z1 in range(0, 5):
+    print('sweet %s' % (z1 + 1))
+
+
+list_products_1 = ['apples', 'tomato', 'duck', 'long_loaf', 'pork',\
+                   'mushrooms', 'beef', 'potato', 'pasta']
+
+for q in list_products_1:
+    print(list([q]))
+for q1 in list_products_1:
+    print(list(q1))
+for q2 in list_products_1:
+    print(q2)
+
+list_name = ['one', 'two', 'three']
+for i in list_name:
+    print('1 %s' % i)
+    for j in list_name:
+        print('2 %s' % j)
+
+
+find_coins = 20
+magic_coins = (10 * 7) #10 coins for seven days per week
+steal_coins = 3
+coins = find_coins
+for week in range(1, 53):
+    coins = coins + magic_coins - steal_coins
+    print('Week %s = %s coins' % (week, coins))
+
