43343ddcb787081810e0ac8a42fa1f6500cf0dab
RARudyk <r.a.rudyk@gmail.com>
2022-02-17 22:32:20 +0200

    learning python restart day 3 while - loop

--- test_start/day_3.py
+++ test_start/day_3.py
@@ -38,3 +38,41 @@ for week in range(1, 53):
     coins = coins + magic_coins - steal_coins
     print('Week %s = %s coins' % (week, coins))
 
+#t1
+for x in range(0, 20):
+    print('Hello %s' % x)
+    if x < 9:
+        break
+
+#t2
+age = 0
+while age < 38:
+    age = age + 2
+    print(age)
+
+# or
+increase = 2    #create first variable for increase age on 2 years
+age_begin = 0   #create second variable for start position for age
+age_end = age_begin #create variable "end_age" as redefine variable "age_begine"
+for age_1 in range(0, 38):
+    age_end = age_end + increase #increase redefined variable "end_age"
+    if age_end <= 38:
+        print(age_end)
+    else:
+        break
+#t3
+y = 0
+ingredients = ['apple', 'pain', 'peach', 'watermelon', 'strawberry', 'raspberry']
+for x in ingredients:
+    y = y + 1
+    print('%s %s' % (y, x))
+
+#t4
+weight = 30
+weight_on_moon = 0.165
+weight_year = weight
+for year in range(1, 16):
+    weight_year = (weight_year + 1)
+    weight_year_m = weight_year * weight_on_moon
+    print('%s year with weight is %f' % (year, weight_year_m))
+
